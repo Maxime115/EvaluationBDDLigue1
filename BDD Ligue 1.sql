9dc836fe750a6469f9ee8c5b3be8496e205efe1c
Maxime115 <leclercqmaximedu62@gmail.com>
2023-10-19 09:17:07 +0200

    reupload

--- BDD Ligue 1.sql
+++ BDD Ligue 1.sql
@@ -1,171 +1,105 @@
- drop database if exists LFP ;
+DROP DATABASE IF EXISTS LFP;
 
-create database if not exists LFP default character set utf8mb4 collate utf8mb4_unicode_ci;
-use LFP;
+CREATE DATABASE IF NOT EXISTS LFP DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-create table if not exists Club
-(
-   Id_Equipe            int not null auto_increment,
-   Nom_Equipe           varchar(100) not null,
-   Division             boolean
+USE LFP;
 
+CREATE TABLE IF NOT EXISTS Club (
+    Id_Equipe INT NOT NULL AUTO_INCREMENT,
+    Nom_Equipe VARCHAR(100) NOT NULL,
+    Division BOOLEAN,
+    PRIMARY KEY (Id_Equipe)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-   PRIMARY KEY (Id_Equipe)
+CREATE TABLE IF NOT EXISTS Joueur (
+    Id_Joueur INT NOT NULL AUTO_INCREMENT,
+    Id_Equipe INT NOT NULL,
+    Nom_Joueur VARCHAR(100) NOT NULL,
+    Prenom_Joueur VARCHAR(100) NOT NULL,
+    Date_Naissance DATE NOT NULL,
+    Numero_License VARCHAR(10) NOT NULL,
+    Nationalite VARCHAR(100) NOT NULL,
+    PRIMARY KEY (Id_Joueur),
+    FOREIGN KEY (Id_Equipe) REFERENCES Club(Id_Equipe)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-);
-Engine = InnoDB default Charset=utf8mb4 collate= utf8mb4_unicode_ci;
+CREATE TABLE IF NOT EXISTS Matchs (
+    Id_Match INT NOT NULL AUTO_INCREMENT,
+    Date_Match DATE NOT NULL,
+    Heure_Match TIME NOT NULL,
+    PRIMARY KEY (Id_Match)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-create table if not exists Joueur
-(
-   Id_Joueur           int not null auto_increment,
-   Id_Equipe           int not null,
-   Nom_Joueur          varchar(100) not null,
-   Prenom_Joueur       varchar(100) not null,
-   Date_Naissance      date not null,
-   Numero_License      varchar(10) not null,
-   Nationnalite        varchar(100) not null
+CREATE TABLE IF NOT EXISTS Titulaire (
+    Id_Joueur INT NOT NULL,
+    Id_Match INT NOT NULL,
+    PRIMARY KEY (Id_Joueur, Id_Match),
+    FOREIGN KEY (Id_Joueur) REFERENCES Joueur(Id_Joueur),
+    FOREIGN KEY (Id_Match) REFERENCES Matchs(Id_Match)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+CREATE TABLE IF NOT EXISTS Banc_Touche (
+    Id_Joueur INT NOT NULL,
+    Id_Match INT NOT NULL,
+    PRIMARY KEY (Id_Joueur, Id_Match),
+    FOREIGN KEY (Id_Joueur) REFERENCES Joueur(Id_Joueur),
+    FOREIGN KEY (Id_Match) REFERENCES Matchs(Id_Match)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+CREATE TABLE IF NOT EXISTS Matchs (
+    Id_Match INT NOT NULL AUTO_INCREMENT,
+    Date_Match DATE NOT NULL,
+    Heure_Match TIME NOT NULL,
+    PRIMARY KEY (Id_Match)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-   PRIMARY KEY (Id_Joueur)
+CREATE TABLE IF NOT EXISTS Fait_Match (
+    Id_Match INT NOT NULL,
+    Id_But INT NOT NULL,
+    Id_Carton INT NOT NULL,
+    PRIMARY KEY (Id_Match, Id_But, Id_Carton),
+    FOREIGN KEY (Id_Match) REFERENCES Matchs(Id_Match),
+    FOREIGN KEY (Id_But) REFERENCES But(Id_But),
+    FOREIGN KEY (Id_Carton) REFERENCES Carton(Id_Carton)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-);
-Engine = InnoDB default Charset=utf8mb4 collate= utf8mb4_unicode_ci;
+CREATE TABLE IF NOT EXISTS But (
+    Id_But INT NOT NULL AUTO_INCREMENT,
+    Id_Match INT NOT NULL,
+    Heure_But TIME NOT NULL,
+    CSC BOOLEAN NOT NULL,
+    PRIMARY KEY (Id_But),
+    FOREIGN KEY (Id_Match) REFERENCES Matchs(Id_Match)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-create table if not exists Titulaire
-(
-   Id_Joueur           int not null,
-   Id_Match           int not null
-   
-   PRIMARY KEY (Id_Joueur, Id_Match)
+CREATE TABLE IF NOT EXISTS Carton (
+    Id_Carton INT NOT NULL AUTO_INCREMENT,
+    Id_Match INT NOT NULL,
+    Heure_Carton TIME NOT NULL,
+    Couleur_Carton BOOLEAN NOT NULL,
+    PRIMARY KEY (Id_Carton),
+    FOREIGN KEY (Id_Match) REFERENCES Matchs(Id_Match)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-);
-Engine = InnoDB default Charset=utf8mb4 collate= utf8mb4_unicode_ci;
+CREATE TABLE IF NOT EXISTS Marque (
+    Id_Joueur INT NOT NULL,
+    Id_But INT NOT NULL,
+    PRIMARY KEY (Id_Joueur, Id_But),
+    FOREIGN KEY (Id_Joueur) REFERENCES Joueur(Id_Joueur),
+    FOREIGN KEY (Id_But) REFERENCES But(Id_But)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-create table if not exists Banc_Touche
-(
-   Id_Joueur           int not null,
-   Id_Match           int not null
-   
-   PRIMARY KEY (Id_Joueur, Id_Match)
+CREATE TABLE IF NOT EXISTS Recoit (
+    Id_Joueur INT NOT NULL,
+    Id_Carton INT NOT NULL,
+    PRIMARY KEY (Id_Joueur, Id_Carton),
+    FOREIGN KEY (Id_Joueur) REFERENCES Joueur(Id_Joueur),
+    FOREIGN KEY (Id_Carton) REFERENCES Carton(Id_Carton)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-);
-Engine = InnoDB default Charset=utf8mb4 collate= utf8mb4_unicode_ci;
-
-create table if not exists Matchs
-(
-   Id_Match           int not null auto_increment,
-   Date_Match         date not null,
-   Heure_Match         time not null
-
-
-   PRIMARY KEY (Id_Match)
-
-);
-Engine = InnoDB default Charset=utf8mb4 collate= utf8mb4_unicode_ci;
-
-create table if not exists Fait_Match
-(
-   Id_Match          int not null,
-   Id_But           int not null,
-   Id_Carton           int not null
-
-   PRIMARY KEY (Id_Match, Id_But, Id_Carton)
-
-);
-Engine = InnoDB default Charset=utf8mb4 collate= utf8mb4_unicode_ci;
-
-create table if not exists But
-(
-   Id_But             int not null auto_increment,
-   Id_Match           int not null,
-   Heure_But         time not null,
-   CSC               boolean not null
-
-   PRIMARY KEY (Id_But)
-
-);
-Engine = InnoDB default Charset=utf8mb4 collate= utf8mb4_unicode_ci;
-
-create table if not exists Carton
-(
-   Id_Carton             int not null auto_increment,
-   Id_Match              int not null,
-   Heure_Carton          time not null,
-   Couleur_Carton        boolean not null
-
-   PRIMARY KEY (Id_Carton)
-
-);
-Engine = InnoDB default Charset=utf8mb4 collate= utf8mb4_unicode_ci;
-
-create table if not exists Marque
-(
-   Id_Joueur           int not null,
-   Id_But           int not null
-   
-   PRIMARY KEY (Id_Joueur, Id_But)
-
-);
-Engine = InnoDB default Charset=utf8mb4 collate= utf8mb4_unicode_ci;
-
-create table if not exists Reçoit
-(
-   Id_Joueur           int not null,
-   Id_Carton           int not null
-   
-   PRIMARY KEY (Id_Joueur, Id_Carton)
-
-);
-Engine = InnoDB default Charset=utf8mb4 collate= utf8mb4_unicode_ci;
-
-create table if not exists Feuille_Match
-(
-   Id_Feuille_Match   int not null auto_increment, 
-   Id_Match           int not null
-   
-   PRIMARY KEY (Id_Feuille_Match)
-
-);
-Engine = InnoDB default Charset=utf8mb4 collate= utf8mb4_unicode_ci;
-
-
-
-ALTER TABLE Joueur
-ADD FOREIGN KEY (Id_Equipe) REFERENCES Club(Id_Equipe);
-
-
-ALTER TABLE Titulaire
-ADD FOREIGN KEY (Id_Joueur) REFERENCES Joueur(Id_Joueur),
-ADD FOREIGN KEY (Id_Match) REFERENCES Matchs(Id_Match);
-
-ALTER TABLE Banc_Touche
-ADD FOREIGN KEY (Id_Joueur) REFERENCES Joueur(Id_Joueur),
-ADD FOREIGN KEY (Id_Match) REFERENCES Matchs(Id_Match);
-
-
-ALTER TABLE Fait_Match
-ADD FOREIGN KEY (Id_Match) REFERENCES Matchs(Id_Match),
-ADD FOREIGN KEY (Id_But) REFERENCES But(Id_But),
-ADD FOREIGN KEY (Id_Carton) REFERENCES Carton(Id_Carton);
-
-ALTER TABLE But
-ADD FOREIGN KEY (Id_Match) REFERENCES Matchs(Id_Match);
-
-ALTER TABLE Carton
-ADD FOREIGN KEY (Id_Match) REFERENCES Matchs(Id_Match);
-
-
-ALTER TABLE Marque
-ADD FOREIGN KEY (Id_Joueur) REFERENCES Joueur(Id_Joueur),
-ADD FOREIGN KEY (Id_But) REFERENCES But(Id_But);
-
-ALTER TABLE Reçoit
-ADD FOREIGN KEY (Id_Joueur) REFERENCES Joueur(Id_Joueur),
-ADD FOREIGN KEY (Id_Carton) REFERENCES Carton(Id_Carton);
-
-
-ALTER TABLE Feuille_Match
-ADD FOREIGN KEY (Id_Match) REFERENCES Matchs(Id_Match);
-
-
+CREATE TABLE IF NOT EXISTS Feuille_Match (
+    Id_Feuille_Match INT NOT NULL AUTO_INCREMENT,
+    Id_Match INT NOT NULL,
+    PRIMARY KEY (Id_Feuille_Match),
+    FOREIGN KEY (Id_Match) REFERENCES Matchs(Id_Match)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE utf8mb4_unicode_ci;
